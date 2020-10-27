386768b0f6afae9890ae9cf3f00292a81f51ed2f
WUI YANG TAN <wuiyang@live.com>
2020-10-28 00:12:32 +0800

    modified dev env

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,19 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            assets: './assets',
+            components: './components',
+            constants: './constants',
+            helpers: './helpers',
+            models: './models',
+          },
+        },
+      ],
+    ],
   };
 };
