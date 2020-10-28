87d3578a6cae4673f8270dfab6439ed2bf59a148
WUI YANG TAN <wuiyang@live.com>
2020-10-28 14:55:27 +0800

    changed cases to follow TS naming convention

--- helpers/CountryEmojiConverter.ts
+++ helpers/CountryEmojiConverter.ts
@@ -1,7 +1,7 @@
 // https://medium.com/binary-passion/lets-turn-an-iso-country-code-into-a-unicode-emoji-shall-we-870c16e05aad
 
 // 127397 = regional indicator A (127,462) - A (65)
-export default function ToCountryEmoji(countryIsoCode: string) {
+export default function toCountryEmoji(countryIsoCode: string) {
   if (countryIsoCode == null || countryIsoCode.length !== 2) {
     return '';
   }
