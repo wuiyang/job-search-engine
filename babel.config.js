f9c7df9fb2916b4ec11fdd85056fa05008dc4627
WUI YANG TAN <wuiyang@live.com>
2020-10-31 23:37:55 +0800

    added screens alias for import

--- babel.config.js
+++ babel.config.js
@@ -13,6 +13,7 @@ module.exports = function(api) {
             helpers: './helpers',
             models: './models',
             queries: './queries',
+            screens: './screens',
           },
         },
       ],
